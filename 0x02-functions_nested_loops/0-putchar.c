9165470828e8220b33d1de459a23806f3ba60138
niklausnick <ademolaadebisi59@gmail.com>
2022-06-07 14:26:27 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints main, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('M');
+	_putchar('a');
+	_putchar('i');
+	_putchar('n');
+
+	return (0);
+}
