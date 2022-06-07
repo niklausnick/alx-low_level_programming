a8d0987d56ab688836261bb009c4637cc3b91e4a
niklausnick <ademolaadebisi59@gmail.com>
2022-06-07 15:13:51 -0700

    I sometimes suffer from insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet,
+ * in lowercase,
+ * followed by a new line
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
