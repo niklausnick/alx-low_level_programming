e5d0bb29bdf89e6e9ebf4c1791851783443f927d
niklausnick <ademolaadebisi59@gmail.com>
2022-06-07 16:33:34 -0700

    correction

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -32,12 +32,12 @@ int main(void)
 		printf(", %lu", k1 + (k2 / 1000000000));
 		printf("%lu", k2 % 1000000000);
 		k1 = k1 + j1;
-		ji = k1 - j1;
+		j1 = k1 - j1;
 		k2 = k2 + j2;
 		j2 = k2 - j2;
 	}
 
-	printf('\n');
+	printf("\n");
 
 	return (0);
 }
