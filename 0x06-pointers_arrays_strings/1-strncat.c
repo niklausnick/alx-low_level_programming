bac4bd7adbb4af63ce81300dde430a1523595c59
niklausnick <ademolaadebisi59@gmail.com>
2022-06-16 14:14:54 -0700

    correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,7 +6,7 @@
  * @src: source string
  * @n: number of bytes of str to concatenate
  *
- * Retrun: a pointer to the resulting string dest
+ * Return: a pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
