a224856e79b9ac04ddb994ac90a81b4afd3d0817
niklausnick <ademolaadebisi59@gmail.com>
2022-06-16 14:28:54 -0700

    correction

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,14 +10,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2)
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if (*s1 == '\0')
-		{
-			return (0);
-		}
-		s1++;
-		s2++
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
-	return (*s1 - *s2);
+	return (0);
 }
