d538df8341b712eca6281936f8bae4c03dcf873e
niklausnick <ademolaadebisi59@gmail.com>
2022-06-17 14:26:42 -0700

    correction2

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,19 +6,43 @@
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	int power;
+	int neg;
+	int hold;
 
+	neg = 0;
+	power = 1;
+	hold = n;
 	if (n < 0)
 	{
-		n1 = -n;
 		_putchar('-');
-	} else
-	{
-		n1 = n;
+		neg = 1;
 	}
 
-	if (n1 / 10)
-		print_number(n1 / 10);
+	while (hold > 9 || hold < -9)
+	{
+		power *= 10;
+		hold /= 10;
+	}
 
-	_putchar((n1 % 10) + '0');
+	while (power > 0)
+	{
+		if (power > 9)
+		{
+			if (!neg)
+				_putchar((n / power % 10) + '0');
+			else
+				_putchar((n / power % 10) * -1 + '0');
+
+			power /= 10;
+		}
+		if (power == 1)
+		{
+			if (neg)
+				_putchar((n % 10) * -1 + '0');
+			else
+				_putchar(n % 10 + '0');
+			power = 0;
+		}
+	}
 }
